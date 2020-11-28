c69b278c20a707dc8a3be68ca0b61c4e875e6058
Tony Mederos <tony_mederos@hotmail.com>
2020-11-28 17:16:38 -0600

    Initial Commit

---
+++ Moneta StudioTests/Moneta_StudioTests.swift
@@ -0,0 +1,33 @@
+//
+//  Moneta_StudioTests.swift
+//  Moneta StudioTests
+//
+//  Created by Tony on 11/28/20.
+//
+
+import XCTest
+@testable import Moneta_Studio
+
+class Moneta_StudioTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
